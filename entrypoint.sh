23719ac9d1f330b95f9ca424808653014555f22b
getapp <tech+github@getapp.com>
2016-05-18 07:27:01 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ config=$SPARK_HOME/conf/spark-defaults.conf
 sed -i "s#__spark_app_name__#${SPARK_APP_NAME:-'spark_app'}#" $config
 sed -i "s#__spark_driver_max_result_size__#${SPARK_DRIVER_MAX_RESULTS_SIZE:-'1g'}#" $config
 sed -i "s#__spark_driver_memory__#${SPARK_DRIVER_MEMORY:-'1g'}#" $config
-sed -i "s#__spark_driver_executor_memory__#${SPARK_EXECUTOR_DRIVER_MEMORY:-'1g'}#" $config
+sed -i "s#__spark_executor_memory__#${SPARK_EXECUTOR_MEMORY:-'1g'}#" $config
 
 exec "$@"
